382e63ffa7deaa6bf73e273568ba6b321b841aee
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-24 14:11:41 -0600

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
+
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
-const homeRoutes = require("./homeRoutes.js");
-const dashboardRoutes = require("./dashboardRoutes.js");
+const userRoutes = require("./userRoutes.js");
+const postRoutes = require("./postRoutes.js");
+const commentRoutes = require("./commentRoutes.js");
 
-router.use("/api", apiRoutes);
-router.use("/", homeRoutes);
-router.use("/dashboard", dashboardRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
