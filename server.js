fc5e159a5fdfa52292e31e663758895f7d2ce593
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-24 12:57:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
 const helpers = require('./utils/helpers');
+const hbs = exphbs.create({
+    helpers
+});
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,8 +15,26 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Code pulled from Mini project as a template
 
-app.listen(PORT, () => {
-	console.log(`Server now listening on port ${PORT}!!`);
-	sequelize.sync({ force: false });
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
