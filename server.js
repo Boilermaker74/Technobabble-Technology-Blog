33684374182e45163c29c39d822bf301007fd5f8
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-24 13:34:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Import dependencies and Helpers 
+
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -9,8 +12,13 @@ const hbs = exphbs.create({
 });
 
 
+// The Sequelize connection is imported
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+// Set PORT variable
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,7 +33,7 @@ const sess = {
   store: new SequelizeStore({
     db: sequelize
     checkExpirationInterval: 1000 * 60 * 10, // check time  every 10 minutes
-    expiration: 1000 *3600                //expire after 1 hour 
+    expiration: 1000 *3600                //expire after 1 hour prevent the database from accumulating expired sessions.
   })
 };
 
@@ -37,7 +45,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+// force false prevent dropping and re-creating the tables.
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
